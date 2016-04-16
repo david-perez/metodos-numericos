9f63344786e260e9820c6aa242b0c278430283ab
david-perez <hallaplay835@gmail.com>
2016-04-16 19:30:38 +0200

    Cholesky usando producto escalar built-in + pre-allocation of arrays

--- cholesky.m
+++ cholesky.m
@@ -11,15 +11,16 @@ function [u, A] = cholesky(A, b)
     
     if nargout == 2 % calcular B primero
         for i = 1:n % calcular la columna i
-            s = sum(A(i,1:i-1) .* A(i,1:i-1));
+            s = A(i,1:i-1) * A(i,1:i-1)';
         
             if A(i, i) <= s
                 disp('La matriz A no admite factorización de Cholesky.')
+                return;
             end
             A(i, i) = sqrt(A(i, i) - s);
         
             for j = i+1:n
-                A(j, i) = 1 / A(i,i) * (A(i, j) - sum(A(i,1:i-1) .* A(j,1:i-1)));
+                A(j, i) = 1 / A(i,i) * (A(i, j) - A(i,1:i-1) * A(j,1:i-1)');
             end
         end
     end
@@ -32,17 +33,17 @@ end
 % Resuelve el sistema triangular inferior Bw = b.
 % Precondición: length(b) = size(B, 1).
 function w = solve_for_w(B, b)
-    w = [];
+    w = zeros(size(B, 1), 1); % Dynamic resizing debería ser amortized O(1) si lo programaron bien, pero por si acaso...
     for i = 1:size(B, 1)
-        w(i) = 1 / B(i, i) * (b(i) - sum(B(i, 1:i-1) .* w(1:i-1)));
+        w(i) = 1 / B(i, i) * (b(i) - B(i, 1:i-1) * w(1:i-1));
     end
 end
 
 % Resuelve el sistema triangular superior Bu = w.
 % Precondicion: length(b) = size(B, 1).
 function u = solve_for_u(B, w)
-    u = [];
+    u = zeros(size(B, 1), 1);
     for i = size(B, 1):-1:1
-        u(i) = 1 / B(i, i) * (w(i) - sum(B(i,i+1:end) .* u(i+1:end)));
+        u(i) = 1 / B(i, i) * (w(i) - B(i,i+1:end) * u(i+1:end));
     end
 end
